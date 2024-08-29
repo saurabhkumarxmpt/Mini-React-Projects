4acbcc5988870b33d4f04a1f4e09e0ee1cab5dab
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-08-29 10:36:24 +0530

    some chnage in app file

--- TO-DO-List-App/src/App.jsx
+++ TO-DO-List-App/src/App.jsx
@@ -1,11 +1,40 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setinput] = useState('');
+  const [array,setarray]=useState([]);
 
+    function handleinput(e){
+      setinput(e.target.value)
+    }
+    function handlebutton(){
+      if(input.trim() !== ''){
+        setarray([...array,input]);
+        setinput('')
+      }
+    }
   return (
     <>
-      <h1>this is  a demo</h1>
+    <input 
+    type="text"
+    placeholder='enter something'
+    value={input}
+    className='outline-0 border p-2 my-10 mx-7 rounded-lg'
+    onChange={handleinput}
+    />
+    <button 
+    onClick={() =>  handlebutton()}
+    className='py-2 px-4 bg-green-400 text-white rounded-md'
+    >click here</button>
+    {/* {btn ? <p>{input}</p> : "something wronfg"} */}
+    <ul className='mt-0 ml-14'>
+        {array.map((array, index) => (
+          <li 
+          key={index}
+          className='border max-w-60 mt-5 p-2'
+          >{index+1}.  {array}</li>
+        ))}
+      </ul>
     </>
   )
 }
