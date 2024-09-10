44c71cb2dac3a05fbb701a7143ecd38f9bedd3ac
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-09-10 17:46:10 +0530

    do some extra work on home tab

--- Admin-dashboard/src/Pages/Home/Home.jsx
+++ Admin-dashboard/src/Pages/Home/Home.jsx
@@ -5,31 +5,74 @@ import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { BarChart } from '@mui/x-charts/BarChart';
 import Divider from '@mui/material/Divider';
+import LinearProgress from '@mui/material/LinearProgress';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { Link } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 export default function CardOne() {
   const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
   ' ',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
+  'Day 1',
+  'Day 2',
+  'Day 3',
+  'Day 4',
+  'Day 5',
+  'Day 6',
 ];
 
 const barUdata = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const varvData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const barlabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
+  'Day 1',
+  'Day 2',
+  'Day 3',
+  'Day 4',
+  'Day 5',
+  'Day 6',
+  'Day 7',
 ];
 
+const Progess=({
+  title='make a website for me',
+  proessValue=30,
+})=>{
+  return(
+    <div>
+    <div className='px-9 py-1 '> 
+      <p className='py-2 font-semibold text-slate-500'>{title}</p>
+      <LinearProgress
+       variant="determinate" 
+       value={proessValue}
+       sx={{width:'380px', borderRadius:5, paddingBottom:1, marginBottom:1}}
+       />
+      <ArrowRightIcon
+      sx={{float:'right', marginTop:'-35px'}}
+      />
+    </div>
+    <Divider />
+    </div>
+  )
+}
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Google.com', 159, 56),
+  createData('Microsoft.com', 237, 98),
+  createData('github.com', 262, 76),
+  createData('linkdin.com', 305, 8),
+  createData('stackoverflow.com', 67,8),
+];
   return (
     <div>
         <div>
@@ -67,22 +110,68 @@ const barlabels = [
     width={500}
     height={300}
     series={[
-      { data: pData, label: 'pv' },
-      { data: uData, label: 'uv' },
+      { data: pData, label: 'Gross Sale' },
+      { data: uData, label: 'Low Sale' },
     ]}
     xAxis={[{ scaleType: 'point', data: xLabels }]}
   />
         </div>
         <div>
+        <div className='py-5 px-8 font-semibold fonrt'>
+            <p>EXPENSES CHART</p>
+          </div>
+          <Divider />
         <BarChart
       width={500}
       height={300}
       series={[
-        { data: varvData, label: 'pv', id: 'pvId' },
-        { data: barUdata, label: 'uv', id: 'uvId' },
+        { data: varvData, label: 'Total Expenses', id: 'pvId' },
+        { data: barUdata, label: 'Gross Expenses', id: 'uvId' },
       ]}
       xAxis={[{ data: barlabels, scaleType: 'band' }]}
     />
+        </div>
+        <div>
+        <div className='py-5 px-8 font-semibold fonrt'>
+            <p>Projects Progess</p>
+          </div>
+          <Divider />
+          <Link to='#'><Progess /></Link>
+          <Link to='#'><Progess title='simple website for a clint' proessValue={56} /></Link>
+          <Link to='#'><Progess  title='logo for a company' proessValue={89}/></Link>
+          <Link to='#'><Progess /></Link>
+        </div>
+        <div>
+        <div className='py-5 px-8 font-semibold '>
+            <p>State List</p>
+          </div>
+          <Divider />
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} size="small" aria-label="a dense table">
+        <TableHead sx={{paddingTop:4}}>
+          <TableRow >
+            <TableCell sx={{fontWeight:'bold'}}>Source</TableCell>
+            <TableCell sx={{fontWeight:'bold'}} align="right">Views</TableCell>
+            <TableCell sx={{fontWeight:'bold'}} align="right">Today</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+            
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </div>
         </div>
     </div>
