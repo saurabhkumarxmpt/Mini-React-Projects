3036fefb9b496f6b95a73ead9bc6902fa7719988
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-09-10 21:00:14 +0530

    work on docs page

--- Admin-dashboard/src/Pages/Docs/Docs.jsx
+++ Admin-dashboard/src/Pages/Docs/Docs.jsx
@@ -1,7 +1,43 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 const Docs=()=>{
     return(
         <div>
-            <h1>docs</h1>
+           <div className=" flex w-[1060px] justify-between items-center">
+            <div>
+                <h1 className="text-2xl">My Docs</h1>
+            </div>
+            <div className='flex gap-6 pr-[10px] items-center'> 
+            <div>
+            <TextField
+          id="standard-search"
+          label="Search field"
+          type="search"
+          variant="standard"
+        />
+        <Button variant="outlined" sx={{marginTop:'15px',marginLeft:'20px'}}>Search</Button>
+            </div>
+            <div className='mt-3'>
+                <select name="select" id="name" className='mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'>
+                <option value="volvo">All</option>
+                <option value="saab">Text file</option>
+                <option value="mercedes">Photos</option>
+                <option value="audi">Zip file</option>
+                </select>
+            </div>
+            <div className='mt-4'>
+            <Button variant="contained">Upload</Button>
+            </div>
+            </div>
+           </div>
+           <div className='flex w-[1060px] flex-wrap'>
+            <div>h1</div>
+           </div>
         </div>
     )
 }
---
+++ Admin-dashboard/src/Pages/Docs/DocsComonent.jsx
@@ -0,0 +1,7 @@
+const DocComponent=()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
