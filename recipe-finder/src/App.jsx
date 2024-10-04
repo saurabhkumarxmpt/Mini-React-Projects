d86094d7fa00e67afca85b09b8f1088de43a32b6
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-10-04 08:24:49 +0530

    make a home page of website

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -1,44 +1,11 @@
-import React, { useEffect, useState } from "react";
+import Homepage from "./pages/HomePage"
 
-const App = () => {
-  const [button, setButton] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    if (button !== "") { // Fetch only if the button (input) is not empty
-      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${button}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setData(data.meals); // Update with `data.meals` because the API returns the meals array
-          setLoading(false);
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setLoading(false);
-        });
-    }
-  }, [button]); // Re-run the effect whenever `button` changes
-
-  return (
+const App=()=>{
+  return(
     <div>
-      <input
-        type="text"
-        value={button}
-        onChange={(e) => setButton(e.target.value)}
-      />
-      <h1>Meals</h1>
-      {loading && <p>Loading...</p>}
-      {!loading && data && (
-        <ul>
-          {data.map((meal) => (
-            <li key={meal.idMeal}>{meal.strMeal}</li> // Display the meal names
-          ))}
-        </ul>
-      )}
+      <Homepage />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ recipe-finder/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const SearchPage=()=>{
+    return(
+        <div>
+            <div className="flex justify-center bg-red-300 py-16 text-4xl font-semibold">
+                <h1>Recipe Serach App</h1>
+            </div>
+        </div>
+    )
+}
+
+export default SearchPage
