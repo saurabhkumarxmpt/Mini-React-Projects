5fc243ec7ffefdf9fd29a8938d7f60042284beb7
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-10-05 19:55:26 +0530

    make Ingredient section

--- recipe-finder/src/pages/RecipeDetailPage.jsx
+++ recipe-finder/src/pages/RecipeDetailPage.jsx
@@ -36,10 +36,59 @@ const RecipeDetail=()=>{
                             <a href={recipe.strYoutube} className="text-lg pb-2 text-red-600"><span className="font-semibold text-black">Video Link:</span> Youtube</a>
                         </div>
                      </div>
-                     <div className="bg-red-400 flex justify-around">
-                        <div>
-                            <h1>{recipe.strMeal} Ingredients
+                     <div className="bg-red-400 flex justify-between py-10 px-[170px] ">
+                        <div className="bg-emerald-500 w-full  max-w-[50%] rounded-lg">
+                            <h1 className="text-lg font-semibold text-center py-4 ">{recipe.strMeal} Ingredients
                             </h1>
+
+                            <div className="bg-white justify-between px-20 py-10 rounded-b-lg">
+                                <div className="flex justify-between">
+                                <ul>
+                                    <li>{recipe.strIngredient1}</li>
+                                    <li>{recipe.strIngredient2}</li>
+                                    <li>{recipe.strIngredient3}</li>
+                                    <li>{recipe.strIngredient4}</li>
+                                    <li>{recipe.strIngredient5}</li>
+                                    <li>{recipe.strIngredient6}</li>
+                                    <li>{recipe.strIngredient7}</li>
+                                    <li>{recipe.strIngredient8}</li>
+                                    <li>{recipe.strIngredient9}</li>
+                                    <li>{recipe.strIngredient10}</li>
+                                    <li>{recipe.strIngredient11}</li>
+                                    <li>{recipe.strIngredient12}</li>
+                                    <li>{recipe.strIngredient13}</li>
+                                    <li>{recipe.strIngredient14}</li>
+                                    <li>{recipe.strIngredient15}</li>
+                                    <li>{recipe.strIngredient16}</li>
+                                    <li>{recipe.strIngredient17}</li>
+                                    <li>{recipe.strIngredient18}</li>
+                                    <li>{recipe.strIngredient19}</li>
+                                    <li>{recipe.strIngredient20}</li>
+                                </ul>
+                                <ul>
+                                    <li>{recipe.strMeasure1}</li>
+                                    <li>{recipe.strMeasure2}</li>
+                                    <li>{recipe.strMeasure3}</li>
+                                    <li>{recipe.strMeasure4}</li>
+                                    <li>{recipe.strMeasure5}</li>
+                                    <li>{recipe.strMeasure6}</li>
+                                    <li>{recipe.strMeasure7}</li>
+                                    <li>{recipe.strMeasure8}</li>
+                                    <li>{recipe.strMeasure9}</li>
+                                    <li>{recipe.strMeasure10}</li>
+                                    <li>{recipe.strMeasure11}</li>
+                                    <li>{recipe.strMeasure12}</li>
+                                    <li>{recipe.strMeasure13}</li>
+                                    <li>{recipe.strMeasure14}</li>
+                                    <li>{recipe.strMeasure15}</li>
+                                    <li>{recipe.strMeasure16}</li>
+                                    <li>{recipe.strMeasure17}</li>
+                                    <li>{recipe.strMeasure18}</li>
+                                    <li>{recipe.strMeasure19}</li>
+                                    <li>{recipe.strMeasure20}</li>
+                                </ul>
+                                </div>
+                            </div>
                         </div>
                         <div className="bg-green-500">
                             2 sned
