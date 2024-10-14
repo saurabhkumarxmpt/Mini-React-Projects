76441986d2af22d8ace1760408e18b3cd3e08cbd
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-10-14 21:01:00 +0530

    try to make weather app

--- Simple-Weather-App/src/App.jsx
+++ Simple-Weather-App/src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+import React,{useState,useEffect} from 'react';
+const App=()=>{
+  const Api_key="f9b0de0aaedf4e26b58151616241410";
+  const city="dhampur"
+  useEffect(
+    
   )
 }
 
